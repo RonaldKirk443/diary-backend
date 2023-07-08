14256bd033be49d43146b480637e4dbe1bcd26e3
LubyLub <seyed8erfan8@gmail.com>
2023-07-08 16:50:52 -0400

    Basic Model for Collection
    Basic Model for collection done

---
+++ src/main/java/com/uio443/diarybackend/model/Collection.java
@@ -0,0 +1,67 @@
+package com.uio443.diarybackend.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.uio443.diarybackend.enums.HiddenStatus;
+import com.uio443.diarybackend.primarykeys.CollectionUserId;
+import jakarta.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@IdClass(CollectionUserId.class)
+@Table(name = "collection")
+public class Collection {
+    // ID collection
+    // ID user
+    // title, description, background img, status
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    Long id;
+    @Id
+    @JoinColumn(name = "userId", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    User user;
+    String title;
+    String backgroundImgLink;
+    HiddenStatus hiddenStatus;
+
+    public Collection() {
+        this.hiddenStatus = HiddenStatus.Private;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBackgroundImgLink() {
+        return backgroundImgLink;
+    }
+
+    public void setBackgroundImgLink(String backgroundImgLink) {
+        this.backgroundImgLink = backgroundImgLink;
+    }
+
+    public HiddenStatus getHiddenStatus() {
+        return hiddenStatus;
+    }
+
+    public void setHiddenStatus(HiddenStatus hiddenStatus) {
+        this.hiddenStatus = hiddenStatus;
+    }
+}
